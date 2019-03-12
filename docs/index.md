cfd1a2aa06fae24080756edbd00d0f46d3763b53
paulwalk <paul@paulwalk.net>
2019-03-12 15:23:02 +0000

    built script to genetrate basic docs

--- docs/index.md
+++ docs/index.md
@@ -1,3 +1,39 @@
 # UNDER DEVELOPMENT!
-<table><thead><tr><th>Name</th><th>Data Type</th><th>Cardinality</th><th>Notes</th></tr></thead><tbody><tr><td>contact</td><td>Nested Data Structure</td><td>Exactly One</td><td></td></tr><tr><td>created</td><td>DateTime</td><td>Exactly One</td><td></td></tr><tr><td>description</td><td>String</td><td>Zero or One</td><td></td></tr><tr><td>ethical_issues_exist</td><td>Term from Controlled Vocabulary</td><td>Exactly One</td><td>from a 'dictionary' containing only: 'yes'; 'no'; 'unknown'</td></tr><tr><td>modified</td><td>DateTime</td><td>Exactly One</td><td></td></tr><tr><td>project</td><td>Nested Data Structure</td><td>Zero or More</td><td></td></tr><tr><td>title</td><td>String</td><td>Zero or One</td><td></td></tr></tbody></table>
+* [DMP](#DMP)
+  * [contact](#contact)
+  * [created](#created)
+  * [description](#description)
+  * [ethical_issues_exist](#ethical_issues_exist)
+  * [modified](#modified)
+  * [project](#project)
+    * [funder_id](#funder_id)
+    * [funding_status](#funding_status)
+    * [grant_id](#grant_id)
+  * [title](#title)
+
+<hr/>
+
+## All Properties
+
+<table>
+  <thead>
+    <tr>
+      <th>Name</th>
+      <th>Data Type</th>
+      <th>Cardinality</th>
+      <th>Notes</th>
+    </tr>
+  </thead>
+  <tbody><tr><td><span id="contact">contact</span></td><td>Nested Data Structure</td><td>Exactly One</td><td> </td></tr>
+<tr><td><span id="created">created</span></td><td>DateTime</td><td>Exactly One</td><td> </td></tr>
+<tr><td><span id="description">description</span></td><td>String</td><td>Zero or One</td><td> </td></tr>
+<tr><td><span id="ethical_issues_exist">ethical_issues_exist</span></td><td>Term from Controlled Vocabulary</td><td>Exactly One</td><td>from a 'dictionary' containing only: 'yes'; 'no'; 'unknown'</td></tr>
+<tr><td><span id="modified">modified</span></td><td>DateTime</td><td>Exactly One</td><td> </td></tr>
+<tr><td><span id="project">project</span></td><td>Nested Data Structure</td><td>Zero or More</td><td> </td></tr>
+<tr><td><span id="title">title</span></td><td>String</td><td>Zero or One</td><td> </td></tr>
+</tbody>
+</table>
+<hr/>
+
+
 this is the footer...
--- scripts/lib/doc_md.rb
+++ scripts/lib/doc_md.rb
@@ -1,4 +1,6 @@
 require 'sequel'
+require 'nokogiri'
+require 'rubytree'
 
 class MarkdownDocument
 
@@ -11,33 +13,100 @@ class MarkdownDocument
     '1..n' => 'One or More'
   }
 
+  INDENT_SIZE = 2
+
   def initialize(path,header,footer,root_property)
     @path = path
     @header = header
     @footer = footer
     @content = ''
     @root_property = root_property
+    @root_node = build_tree(@root_property)
   end
 
   def generate
-    @content += "<table><thead><tr>"
-    PROPERTY_COLUMNS.each {|col| @content += "<th>#{col}</th>"}
-    @content += "</tr></thead>"
-    @content += "<tbody>"
-    @root_property.children.each do |property|
-      @content += "<tr>"
-      @content += "<td>#{property.label_machine}</td>"
-      @content += "<td>#{property.data_type.label}</td>"
-      @content += "<td>#{CARDINALITY_LABELS[property.cardinality]}</td>"
-      notes = property.notes
-      if !notes | notes == '' then
-        notes = '&nbsp;'
-      end
-      @content += "<td>#{notes}</td>"
-      @content += "</tr>"
-    end
-    @content += "</tbody></table>"
+    @content += generate_html_tree(@root_node)
+    @content += "\n<hr/>\n\n"
+    @content += "## All Properties\n\n"
+    @content += generate_table(@root_node.children)
+    @content += "\n<hr/>\n\n"
   end
+
+  def generate_nested_li(node)
+    li = ('  '*node.node_depth)
+    li += "* [#{node.name}](##{node.name})\n"
+    return li
+  end
+
+  def generate_html_tree(node)
+    html_tree = ''
+    node.print_tree(node.node_depth,nil,lambda { |node, prefix| html_tree += "#{generate_nested_li(node)}" })
+    return html_tree
+  end
+
+  def generate_table(node_array)
+    table_content = "<table><thead><tr>"
+    PROPERTY_COLUMNS.each {|col| table_content += "<th>#{col}</th>"}
+    table_content += "</tr></thead>"
+    table_content += "<tbody>"
+    node_array.each do |node|
+      table_content += "#{get_node_as_html_table_row(node)}\n"
+    end
+    table_content += "</tbody></table>"
+    return get_formatted_html(table_content)
+  end
+
+  def get_formatted_html(raw_html)
+    doc = Nokogiri::XML::DocumentFragment.parse(raw_html)
+    return doc.to_xml(indent:INDENT_SIZE)
+  end
+
+
+  def build_tree(root_property)
+    root_node = Tree::TreeNode.new("DMP", {})
+    root_node = add_node_to_parent(root_node,root_property)
+    return root_node
+  end
+
+  def add_node_to_parent(parent_node,parent_property)
+    parent_property.children.each do |property|
+      new_node = Tree::TreeNode.new(property.label_machine,get_property_attributes_as_hash(property))
+      new_node.content['depth'] = new_node.node_depth
+      parent_node << new_node
+      if(!property.children.empty?) then
+        property.children.each do |child_property|
+          add_node_to_parent(new_node,child_property)
+        end
+      end
+    end
+    return parent_node
+  end
+
+  def get_property_attributes_as_hash(property)
+    {
+      :data_type => property.data_type.label,
+      :cardinality => CARDINALITY_LABELS[property.cardinality],
+      :notes => property.notes
+    }
+  end
+
+  def get_node_as_html_table_row(node)
+    html = "<tr><td><span id=\"#{node.name}\">#{node.name}</span></td>"
+    html += "<td>#{process_content_for_table_cell(node.content[:data_type])}</td>"
+    html += "<td>#{process_content_for_table_cell(node.content[:cardinality])}</td>"
+    html += "<td>#{process_content_for_table_cell(node.content[:notes])}</td>"
+    html += "</tr>"
+    return html
+  end
+
+  def process_content_for_table_cell(content)
+    if(content == nil || content == '') then
+      return ' '
+    else
+      return content
+    end
+  end
+
 
   def write_to_file
     File.open(@path,'w') do |f|
