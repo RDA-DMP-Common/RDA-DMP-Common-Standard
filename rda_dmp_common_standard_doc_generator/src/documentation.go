a69f567abca6e67f3f92f91e932f80b5924296ed
emily0c <169072827+emily0c@users.noreply.github.com>
2024-07-31 12:24:52 -0400

    Update documentation.go added fontsize changes and centering

--- rda_dmp_common_standard_doc_generator/src/documentation.go
+++ rda_dmp_common_standard_doc_generator/src/documentation.go
@@ -12,7 +12,7 @@ import (
 
 func generateDoc() {
 	content := "<center><h2 style='color: red;'>UNDER DEVELOPMENT</h2></center>"
-	content += fmt.Sprintf("<h1>%s</h1>", config.DocTitle)
+	content += fmt.Sprintf("<center><h1 style='font-size: 1.2em;'>%s</h1></center>", config.DocTitle)
 	content += "<table id=\"table1\"><tr><td valign=\"top\">"
 	var entityDescriptions []EntityDescription
 	db.Find(&entityDescriptions)
